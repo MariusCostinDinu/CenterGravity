702e2fe5320e3ff8dedcba43fe003e20114a2195
MariusDinu <doskyas@gmail.com>
2020-03-15 08:48:17 +0200

    Explosions

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -5,18 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Asteroid : MonoBehaviour
 {
+    public GameObject particles;
     public float aSpeed = 1.0f;
     void Start()
     {
         float rng = UnityEngine.Random.Range(0.5f,1.5f);
         transform.localScale = new Vector3 (rng, rng, rng);
-
     }
 
-    // Update is called once per frame
     void Update()
-    {
-       
+    {       
         MoveAsteroidToPlayer();
     }
 
@@ -28,12 +26,19 @@ public class Asteroid : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Photon"))
+        {
+            SpawnParticle(collision.transform.position);
             Destroy(collision.gameObject);
+        }
 
         if (collision.gameObject.CompareTag("Player"))
             SceneManager.LoadScene("MainLevel");
     }
 
-   
+    void SpawnParticle(Vector2 TempPosition)
+    {
+        Instantiate(particles, TempPosition, Quaternion.identity);
+    }
+
 
 }
